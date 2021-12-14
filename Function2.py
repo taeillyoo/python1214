c8028ce223cfd7b8fee1d1d256e04ef2147af6af
taeillyoo <taeill15@gmail.com>
2021-12-14 11:01:26 +0900

    lamda function practice

--- Function2.py
+++ Function2.py
@@ -81,3 +81,11 @@ print(userURIBuilder("credu.com", "80", id="kim", name="mike"))
 print(userURIBuilder("credu.com", "80", id="kim", name="mike", 
     age = "30"))
 
+
+# lamda function (nonamed concise function)
+g = lambda x, y: x*y
+print(g(3, 4))
+print(g(5, 6))
+print( (lambda x: x*x)(3))
+print( globals())
+
