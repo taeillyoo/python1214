4d7a34cc6cc5c4941ffef8e6e17531b6228cef27
taeillyoo <taeill15@gmail.com>
2021-12-14 14:50:59 +0900

    memory management

--- 생성자소멸자01.py
+++ 생성자소멸자01.py
@@ -1,8 +1,20 @@
 # -*- 생성자와 소멸자 -*-
 class MyClass:
+    # constructor method (responsible for initialization)
     def __init__(self, value):
         self.value = value
         print("Instace is created! value = ", value)
+    # destructor method (executed the last)
     def __del__(self):
         print("Instance is deleted!")
+
+# generating an instance
+d = MyClass(5)
+
+# memory management is automatic
+# del d
+
+print("[+] exited all code executions")
+
+# garbage collector is executed periodically.
 
