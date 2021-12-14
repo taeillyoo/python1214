cec7d6279df45cdfa2471ee6b64fae06a20f9060
taeillyoo <taeill15@gmail.com>
2021-12-14 14:36:50 +0900

    instance practice

--- Person2.py
+++ Person2.py
@@ -1,31 +1,20 @@
-# class1.py
-
+# Person2.py 
 class Person:
-    # initial method
+    #클래스 멤버 변수
+    num_person = 0 
     def __init__(self):
         self.name = "default name"
+        Person.num_person += 1 
     def print(self):
         print("My name is {0}".format(self.name))
 
 
-# generating an instance
 p1 = Person()
-p1.name = "second name"
-
 p2 = Person()
-
-p1.print()
-p2.print()
-
-# add a member in runtime
+print("숫자:{0}".format(Person.num_person))
 Person.title = "new title"
 print(p1.title)
 print(p2.title)
 print(Person.title)
-
-# add a member to the an instance
-p1.age = 30
-print(p1.age)
-# print(p2.age)
 
 
