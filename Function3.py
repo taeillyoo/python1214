275647c7d1f91c888bb9a45cd5dde3049940d12b
taeillyoo <taeill15@gmail.com>
2021-12-14 13:10:54 +0900

    iterate function practice

--- Function3.py
+++ Function3.py
@@ -7,3 +7,27 @@ print(result)
 years = list(range(2000, 2022))
 print(years)
 
+# minus -1
+numbers = list(range(10, 0, -1))
+print(numbers)
+
+# list comprehension
+lst = list(range(1,11))
+result = [i**2 for i in lst if i>5]
+print(result)
+
+# filtering function
+def getBiggerThan20(i):
+    return i>20
+
+# printing
+lst = [10, 25, 30]
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print(item)
+
+
+print("--- lambda function ---")
+iterL = filter(lambda x: x>20, lst)
+for item in iterL:
+    print(item)
